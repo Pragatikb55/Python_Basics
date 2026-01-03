7fb73488b0bea15739ded9f4a82662afb1c4d93a
Pragatikb55 <pragatik194@gmail.com>
2026-01-03 15:25:02 +0530

    Added loops basic program

---
+++ PythonBasic.py
@@ -0,0 +1,37 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Name:", name)
+print("Age:", age)
+
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+
+for i in range(1, 6):
+    print(i)
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print("Sum:", result)
+numbers = [10, 20, 30, 40]
+numbers.append(50)
+numbers.remove(20)
+print(numbers)
+
+with open("sample.txt", "w") as f:
+    f.write("Hello Python")
+with open("sample.txt", "r") as f:
+    print(f.read())
